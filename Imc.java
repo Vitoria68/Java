c1d08992f33d77596d5d913c7ca7b2b8d3b35d06
Vitória 3 <96185574+Vitoria68@users.noreply.github.com>
2024-04-06 19:05:20 -0300

    Create Imc.java

---
+++ Imc.java
@@ -0,0 +1,30 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Imc {
+
+	public static void main(String[] args) {
+    Scanner teclado = new Scanner(System.in);
+    
+    double imc, peso, altura;
+    
+    System.out.println("************* CALCULADORA *************");
+    System.out.println("****************** DE *****************");
+    System.out.println("****************** IMC ****************");
+    
+    System.out.print("\nInforme o seu peso para cálculo do IMC: ");
+    peso = teclado.nextDouble();
+    
+    System.out.print("\nInforme a sua altura para cálculo do IMC: ");
+    altura = teclado.nextDouble();
+    
+    
+    imc = peso/(altura * altura);
+  
+    System.out.printf("\nO valor do seu IMC é: %.2f", imc);
+    
+    teclado.close();
+	}
+
+}
