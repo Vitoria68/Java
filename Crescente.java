54ebe5f0b2ea6ffb5e7f0d6ae60528c2ce1d2bdf
Vitória 3 <96185574+Vitoria68@users.noreply.github.com>
2024-04-18 19:51:54 -0300

    Create Crescente.java

---
+++ Crescente.java
@@ -0,0 +1,31 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Crescente {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x, y;
+		
+		System.out.print("Digite dois numeros: ");
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		while ( x != y ) {
+			    if ( x > y) {
+			       System.out.println("DECRESCENTE!");
+		       }
+		       else if ( x < y ) {
+			            System.out.println("CRESCENTE");
+		       }
+			    System.out.print("Digite dois numeros: ");
+				x = sc.nextInt();
+				y = sc.nextInt();
+	    }
+		
+		sc.close();
+	}
+
+}
