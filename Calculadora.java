1e5025a3f0fd7e111a259a630811543e3fa646cd
Vitória 3 <96185574+Vitoria68@users.noreply.github.com>
2024-07-22 09:26:34 -0300

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,41 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double n1, n2, resultado = 0;
+		String op;
+		
+		System.out.println("Informe o primeiro numero: ");
+		n1 = sc.nextDouble(); 
+		System.out.println("Informe o segundo numero: ");
+		n2 = sc.nextDouble();
+		System.out.print("Informe a operação ( +, -, *, / ): ");
+		op = sc.next();
+		
+		switch(op) {
+		  case "+":
+			  resultado = n1 + n2;
+			  break;
+		  case "-":
+			  resultado = n1 - n2;
+			  break;
+		  case "*":
+			  resultado = n1 * n2;
+			  break;
+		  case "/":
+			  resultado = n1 / n2;
+			  break;
+		}
+		
+		
+		System.out.println("Resultado da operação: " + resultado);
+		
+		sc.close();
+	}
+
+}
